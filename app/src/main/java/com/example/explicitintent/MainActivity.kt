82060fc76b902f3f4a56c15d035400d4b5691c43
saigithubdcor <saim.dcor@gmail.com>
2023-03-30 10:30:28 +0530

    Adding files of the project

---
+++ app/src/main/java/com/example/explicitintent/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.explicitintent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+   val btnIntent= findViewById<Button>(R.id.button)
+
+        btnIntent.setOnClickListener {
+            // Open a new activity
+
+            intent= Intent(applicationContext, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
